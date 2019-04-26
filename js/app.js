294afecc7a8301322685e324a27ebe3203ce5802
albertoleonm <albertoleonm21@gmail.com>
2019-04-26 14:02:33 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+class Product {
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+// UI Constructor
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong class="product"><i class="fas fa-box"></i> Product</strong>: ${product.name} -
+                    <strong class="price"><i class="fas fa-coins"></i> Price</strong>: $ ${product.price} - 
+                    <strong class="year"><i class="far fa-calendar-alt"></i> Year</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete"><i class="fas fa-trash"></i> Delete</a>
+                </div>
+            </div>
+        `;
+        productList.appendChild(element);
+    }
+
+    resetForm() {
+        document.getElementById('product-form').reset();
+    }
+
+    deleteProduct(element) {
+        if (element.name === 'delete') {
+            element.parentElement.parentElement.remove();
+            this.showMessage('Product Deleted Successfully', 'danger');
+        }
+    }
+
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-2`;
+        div.appendChild(document.createTextNode(message));
+        // Show in The DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        // Insert Message in the UI
+        container.insertBefore(div, app);
+        // Remove the Message after 3 seconds
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 5000);
+    }
+}
+
+// DOM Events
+document.getElementById('product-form')
+    .addEventListener('submit', function (e) {
+
+        const name = document.getElementById('name').value,
+            price = document.getElementById('price').value,
+            year = document.getElementById('year').value;
+
+        // Create a new Oject Product
+        const product = new Product(name, price, year);
+
+        // Create a new UI
+        const ui = new UI();
+
+        // Input User Validation
+        if (name === '' || price === '' || year === '') {
+            ui.showMessage('You should insert data in the fields', 'danger');
+        }
+
+        // Save Product
+        ui.addProduct(product);
+        ui.showMessage(' Product added successfully', 'success');
+        ui.resetForm();
+
+        e.preventDefault();
+    });
+
+document.getElementById('product-list')
+    .addEventListener('click', function (e) {
+        const ui = new UI();
+        ui.deleteProduct(e.target);
+        e.preventDefault();
+    });
